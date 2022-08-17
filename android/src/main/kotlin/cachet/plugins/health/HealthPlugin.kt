fe1ddb6ce3c374f24321aaff0afc07a970ccae34
Cch18 <chuan.hu@outlook.com>
2022-08-17 12:48:17 +0100

    test3

--- android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
+++ android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
@@ -367,9 +367,9 @@ class HealthPlugin(private var channel: MethodChannel? = null) : MethodCallHandl
       .build()
 
     if (dataType == DataType.TYPE_SLEEP_SEGMENT) {
-      Log.i("FLUTTER_HEALTH::SUCCESS", "Hello!!!!!!!!!!")
       typesBuilder.accessSleepSessions(FitnessOptions.ACCESS_READ)
     }
+	Log.i("FLUTTER_HEALTH::SUCCESS", "Hello!!!!!!!!!!")
     val fitnessOptions = typesBuilder.build()
     try {
       val googleSignInAccount =
